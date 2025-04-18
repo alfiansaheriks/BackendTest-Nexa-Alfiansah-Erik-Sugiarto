e91ecfa9cf143f7ea2b48c9c0e599d97218a1a2d
alfiansaheriks <alfiansah.erik.ft20@mail.umy.ac.id>
2025-04-18 21:42:23 +0700

    Add: Store Procedure and View

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
-app.use(routes);
+
+app.use('/api', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
